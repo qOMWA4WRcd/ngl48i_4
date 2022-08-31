7be387014755bc753e9e1f08ff630bb2f0baf489
eA90p <ghd969697@gmail.com>
2022-08-31 19:58:52 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,9 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = "67cOOUROEGg7FApnc_U-ihL43EZGqYsl"
+
+    const api_key = "vi1fRc6cuMcpLzuEcp6_frkY2o_c5IVP"
+
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
@@ -40,7 +42,9 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = "67cOOUROEGg7FApnc_U-ihL43EZGqYsl"
+    const api_key = "vi1fRc6cuMcpLzuEcp6_frkY2o_c5IVP"
+    
+
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
       const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
